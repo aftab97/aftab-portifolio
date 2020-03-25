1a6fd61f03394e90f39cd7746fb37dd752ddfe11
aftab97 <aftabalam.work@gmail.com>
2020-03-25 00:43:45 +0000

    Updates

---
+++ precache-manifest.6101b79db0b943e3852bf8947bed8be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2999cbdccaa9f78e39353adb49b070eb",
+    "url": "/aftab-portifolio/index.html"
+  },
+  {
+    "revision": "52676dc657f8f6caa8b5",
+    "url": "/aftab-portifolio/static/css/main.a31d9310.chunk.css"
+  },
+  {
+    "revision": "f7fd920eb0bd72227646",
+    "url": "/aftab-portifolio/static/js/2.e896946f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aftab-portifolio/static/js/2.e896946f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52676dc657f8f6caa8b5",
+    "url": "/aftab-portifolio/static/js/main.1172eecb.chunk.js"
+  },
+  {
+    "revision": "d2822b9c435d8f8326df",
+    "url": "/aftab-portifolio/static/js/runtime-main.31957b2a.js"
+  },
+  {
+    "revision": "f41fcbdcf14d49aa78c4e52bc9b2a1ab",
+    "url": "/aftab-portifolio/static/media/pp.f41fcbdc.jpg"
+  }
+]);
